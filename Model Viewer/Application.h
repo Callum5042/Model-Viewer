39c1fa9ccefbd1c42383a33ba4da999e1576d984
Callum Anning <CallumAnning@hotmail.co.uk>
2020-12-20 23:36:08 +0000

    Added a few comments

--- Model Viewer/Application.h
+++ Model Viewer/Application.h
@@ -19,23 +19,33 @@ public:
 private:
 	bool m_Running = true;
 	Timer m_Timer;
+
+	// Core functions
+	bool Init();
+	void Render();
+
+	// Dear ImGui rendering function
+	void RenderGui();
+
+	// Calculate FPS
 	int m_FramesPerSecond = 0;
 	void CalculateFramesPerSecond();
 
-	bool Init();
-	void Render();
-	void RenderGui();
-
+	// Window
 	std::unique_ptr<Window> m_Window = nullptr;
+
+	// Event system
 	std::unique_ptr<EventDispatcher> m_EventDispatcher = nullptr;
 
-	void ChangeRenderAPI();
-	RenderAPI m_SwitchRenderAPI = RenderAPI::NONE;
-
+	// Rendering pipeline
 	std::unique_ptr<IRenderer> m_Renderer = nullptr;
 	std::unique_ptr<IShader> m_Shader = nullptr;
 	std::unique_ptr<ICamera> m_Camera = nullptr;
 	std::unique_ptr<IModel> m_Model = nullptr;
+
+	// Rendering API switch
+	void ChangeRenderAPI();
+	RenderAPI m_SwitchRenderAPI = RenderAPI::NONE;
 
 	// Inherited via QuitListener
 	virtual void OnQuit() override;
