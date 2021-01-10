3ae04fb9a1b0250f1f3c5a7efb5642c1b27860a9
Callum Anning <CallumAnning@hotmail.co.uk>
2021-01-10 17:10:08 +0000

    Added assimp dependency

---
+++ Model Viewer/ModelLoader.cpp
@@ -0,0 +1,23 @@
+#include "Pch.h"
+#include "ModelLoader.h"
+
+#undef min
+#undef max
+#include <assimp/Importer.hpp>
+#include <assimp/scene.h>
+#include <assimp/postprocess.h>
+
+bool ModelLoader::Load(const std::string& path, MeshData* meshData)
+{
+	Assimp::Importer importer;
+	auto scene = importer.ReadFile(path, aiProcessPreset_TargetRealtime_Fast | aiProcess_ConvertToLeftHanded | aiProcess_PopulateArmatureData);
+
+	// Load model
+	if (!scene || scene->mFlags & AI_SCENE_FLAGS_INCOMPLETE || !scene->mRootNode)
+	{
+		std::cerr << "ERROR::ASSIMP::" << importer.GetErrorString() << '\n';
+		throw std::runtime_error("Could not load model");
+	}
+
+	return true;
+}
