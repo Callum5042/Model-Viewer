1c3ae7b8170d08aeba18d69992308ff3068eb599
Callum Anning <CallumAnning@hotmail.co.uk>
2021-01-10 19:55:10 +0000

    Added normals and tangents to be updated by the bone

--- Model Viewer/Data Files/Shaders/VertexShader.hlsl
+++ Model Viewer/Data Files/Shaders/VertexShader.hlsl
@@ -12,7 +12,10 @@ PixelInput main(VertexInput input)
 	weights[3] = 1.0f - weights[0] - weights[1] - weights[2];
 
 	// Transform by bone influence
-	float3 position = float3(0, 0, 0);
+	float3 position = float3(0.0f, 0.0f, 0.0f);
+	float3 normal = float3(0.0f, 0.0f, 0.0f);
+	float3 tangent = float3(0.0f, 0.0f, 0.0f);
+	float3 bi_tangent = float3(0.0f, 0.0f, 0.0f);
 	for (int i = 0; i < 4; i++)
 	{
 		float weight = weights[i];
@@ -20,6 +23,9 @@ PixelInput main(VertexInput input)
 		matrix transform = cBoneTransform[bone_index];
 
 		position += weight * mul(float4(input.Position, 1.0f), transform).xyz;
+		normal += weight * mul(input.Normal, (float3x3)transform);
+		tangent += weight * mul(input.Tangent.xyz, (float3x3)transform);
+		bi_tangent += weight * mul(input.BitTangent.xyz, (float3x3)transform);
 	}
 
 	// Transform to homogeneous clip space.
@@ -28,7 +34,7 @@ PixelInput main(VertexInput input)
 	output.PositionH = mul(output.PositionH, cProjection);
 
 	// Transform to world space.
-	output.Position = mul(float4(input.Position, 1.0f), cWorld).xyz;
+	output.Position = mul(float4(position, 1.0f), cWorld).xyz;
 
 	// Pass colour to pixel shader
 	output.Colour = input.Colour;
@@ -37,12 +43,12 @@ PixelInput main(VertexInput input)
 	output.Texture = mul(float4(input.Texture, 1.0f, 1.0f), cTextureTransform).xy;
 
 	// Transform normals by inverse world
-	output.Normal = mul(input.Normal, (float3x3)cInverseWorld).xyz;
+	output.Normal = mul(normal, (float3x3)cInverseWorld).xyz;
 	output.Normal = normalize(output.Normal);
 
 	// Normal mapping
-	output.Tangent = mul(input.Tangent, (float3x3)cInverseWorld);
-	output.BitTangent = mul(input.BitTangent, (float3x3)cInverseWorld);
+	output.Tangent = mul(tangent, (float3x3)cInverseWorld);
+	output.BitTangent = mul(bi_tangent, (float3x3)cInverseWorld);
 
 	output.Tangent = normalize(output.Tangent);
 	output.BitTangent = normalize(output.BitTangent);
